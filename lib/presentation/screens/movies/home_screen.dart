ba6216c3e66de043bd401f6c07af2ef17a1784c1
NehemiasR <nehemis16@gmail.com>
2024-02-02 21:44:51 -0600

    icono cinemapedia icono busqueda quede en el video en el minuto 7:02

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:cinemapedia/presentation/providers/movies/movies_providers.dart';
+import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -33,12 +34,21 @@ class _HomeViewState extends ConsumerState<_HomeView> {
   @override
   Widget build(BuildContext context) {
     final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
-    return ListView.builder(
-      itemCount: nowPlayingMovies.length,
-      itemBuilder: (context, index) {
-        final movie = nowPlayingMovies[index];
-        return ListTile(title: Text(movie.title));
-      },
+    return Column(
+      children: [
+        const CustomAppbar(),
+        Expanded(
+          //El expanded lo que hace es que dentro del padre se expanda todo lo posible
+          child: ListView.builder(
+            itemCount: nowPlayingMovies.length,
+            itemBuilder: (context, index) {
+              final movie = nowPlayingMovies[index];
+
+              return ListTile(title: Text(movie.title));
+            },
+          ),
+        )
+      ],
     );
   }
 }
---
+++ lib/presentation/widgets/widgets.dart
@@ -0,0 +1,1 @@
+export 'shared/custon_appbar.dart';
